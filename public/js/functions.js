7d7122de295ab3a7ff6b532f681f6091678fd196
ArsenTech <62609185+ArsenTech@users.noreply.github.com>
2023-02-09 23:31:03 +0400

    Replace Tabs with a Smart Downloader and Minified Some Code

--- public/js/functions.js
+++ public/js/functions.js
@@ -1,9 +1,8 @@
 import {ref, getDownloadURL, getMetadata } from "https://www.gstatic.com/firebasejs/9.13.0/firebase-storage.js";
 import {storage} from "./firebase.js";
 function displayCards(cards, cardList){
-     cardList.innerHTML = cards.map((card, index)=>{
-          return `
-          <div class="grid-item" data-item="${card.category}" data-index="${index}" data-file-path="${card.filePath}">
+     cardList.innerHTML = cards.map((card, i)=>{
+          return `<div class="grid-item" data-item="${card.category}" data-index="${i}" data-file-path="${card.filePath}">
                <div class="card" style="${card.style}">
                     <div class="card-content">
                              <h2 class="card-title">${card.title}</h2>
@@ -14,27 +13,22 @@ function displayCards(cards, cardList){
                          <button class="card-btn" data-file-path="${card.filePath}"><img src="files/icons/download.svg" alt="download" width="30" height="30">Download</button>
                     </div>
                </div>
-          </div>
-          `
-     }).join("");
+          </div>`}).join("");
 }
 function formatBytes(t,B=2){
-     if(!+t)return"0 Bytes";
-     const o=B<0?0:B,
-     a=Math.floor(Math.log(t)/Math.log(1024));
+     if(!+t)return"0 Bytes";const o=B<0?0:B,a=Math.floor(Math.log(t)/Math.log(1024));
      return`${parseFloat((t/Math.pow(1024,a)).toFixed(o))} ${["Bytes","KB","MB","GB","TB","PB","EB","ZB","YB"][a]}`
 }
 function lazyCss(e) {const t=document.createElement("link");t.href=e,t.rel="stylesheet";t.type="text/css";document.getElementsByTagName("head")[0].appendChild(t);}
 function lazyJS(e){const t=document.createElement("script");t.src=e;t.async=true;t.type="module";document.body.appendChild(t);}
 function downloadFile(file,icon){icon.src = 'files/icons/downloading.svg';getDownloadURL(ref(storage,file)).then(url=>{const link=document.createElement("a");link.href=url;link.download="";link.click();icon.src = 'files/icons/download-done.svg';setTimeout(()=>icon.src='files/icons/download.svg',2000);}).catch((err)=>console.error(err));}
 const downloadWallpaper=(file)=>getDownloadURL(ref(storage,file)).then(url=>window.open(url)).catch((err) => console.error(err));
-function getFileInfo(file,index){
-     const fileRef=ref(storage,file);
-     getMetadata(fileRef).then(data=>{const sizeDisplay=document.querySelectorAll("#size"),dateDisplay=document.querySelectorAll("#updated");sizeDisplay[index].innerHTML=formatBytes(data.size);dateDisplay[index].innerHTML=data.updated.slice(0,10);}).catch((err)=>console.error(err));
-}
-function getWallpaperInfo(f,i){const fileInfoRef = ref(storage, f);getMetadata(fileInfoRef).then(data => {const wallpaperSizeDisplay = document.querySelectorAll("#fileSize");wallpaperSizeDisplay[i].innerHTML = formatBytes(data.size);}).catch((error) => console.error(error));}
-function handleSubmit(e, i){
-     e.preventDefault();const ids=["download-option1", "download-option2", "download-option3", "download-option4", "download-option5"];const selected=document.querySelectorAll(ids.map(id=>`#${id}`).join(", "));const filePath=`wallpapers/coding${i}/${selected[i].value}.png`;downloadWallpaper(filePath);e.target.reset();
-}
-function handleChange(e, i){const filePath = `wallpapers/coding${i}/${e.target.value}.png`;getWallpaperInfo(filePath, i)}
-export {displayCards,lazyCss,lazyJS,downloadFile,downloadWallpaper,getFileInfo,getWallpaperInfo,handleChange,handleSubmit}
+function getFileInfo(file,i){const fileRef=ref(storage,file);getMetadata(fileRef).then(d=>{const sizeDisplay=document.querySelectorAll("#size"),dateDisplay=document.querySelectorAll("#updated");sizeDisplay[i].innerHTML=formatBytes(d.size);dateDisplay[i].innerHTML=d.updated.slice(0,10);}).catch((err)=>console.error(err));}
+function getWallpaperInfo(file){const fileInfoRef = ref(storage, file);getMetadata(fileInfoRef).then(data => {const wallpaperSizeDisplay = document.querySelector("#fileSize");wallpaperSizeDisplay.innerHTML = formatBytes(data.size);}).catch((error) => console.error(error));}
+function changeWallpaper(img){const image = `files/wallpaperImg/coding${img}.webp`,preview = document.getElementById("preview-img");preview.src = image;return img;}
+function changeSize(e, img){const filePath = `wallpapers/coding${img}/${e.target.value}.png`;getWallpaperInfo(filePath, img);}
+function changeSizeBasedOn(size, img){const filePath = `wallpapers/coding${img}/${size}.png`;getWallpaperInfo(filePath, img);}
+function toggleActive(toggler,menu){toggler.classList.toggle("active"); menu.classList.toggle("active");}
+function closeMenu(toggler,menu){toggler.classList.remove("active"); menu.classList.remove("active");}
+function toggleMode(toggler){document.body.classList.toggle("dark");if(!document.body.classList.contains("dark")){toggler.querySelector("img").src = "files/icons/light.svg";localStorage.setItem("arsentech-theme", "light");} else {toggler.querySelector("img").src = "files/icons/dark.svg";localStorage.setItem("arsentech-theme", "dark");}}
+export {displayCards,lazyCss,lazyJS,downloadFile,downloadWallpaper,getFileInfo,getWallpaperInfo,changeSize,changeWallpaper,changeSizeBasedOn,toggleActive,closeMenu,toggleMode}
--- public/js/script.js
+++ public/js/script.js
@@ -1,61 +1,49 @@
 import {downloads} from "./data.js";
-import {displayCards,lazyJS,lazyCss,downloadFile,getFileInfo,handleSubmit,handleChange} from "./functions.js";
+import {displayCards,lazyJS,lazyCss,downloadFile,getFileInfo,changeWallpaper,changeSize,downloadWallpaper,changeSizeBasedOn,toggleActive,closeMenu,toggleMode} from "./functions.js";
 const navbar = document.querySelector( ".navbar" ),
-      toggler = document.querySelector( ".menu-toggler" ),
-      navMenu = document.querySelector( ".navbar-menu" ),
-      downloadsContainer = document.getElementById("downloadsCards"),
-      tabs = document.querySelectorAll(".tab-card .tab-content .tab"),
-      tabHeaderBtns = document.querySelectorAll(".tab-card .tab-header .tab-link"),
-      downloadOptions = document.querySelectorAll(".download-options"),
-      allStar = document.querySelectorAll(".rating .star"),
-      ratingValue = document.getElementById("rating"),
-      ratingError = document.querySelector(".ratingError"),
-      contactForm = document.querySelector(".contact-form"),
-      getMode = localStorage.getItem("arsentech-theme"),
-      modeToggler = document.querySelector("#icon"),
-      navLinks = document.querySelectorAll("#navLinks a"),
-      frmSearch = document.querySelector(".search-container"),
-      cancelSearchBtn = document.querySelector("#cancel-search");
+     toggler = document.querySelector( ".menu-toggler" ),
+     navMenu = document.querySelector( ".navbar-menu" ),
+     downloadsContainer = document.getElementById("downloadsCards"),
+     wallpaperOptions = document.querySelector("#wallpaper-opt"),
+     sizeOptions = document.querySelector("#size-opt"),
+     frmWallpaper = document.getElementById("wp-form"),
+     allStar = document.querySelectorAll(".rating .star"),
+     ratingValue = document.getElementById("rating"),
+     ratingError = document.querySelector(".ratingError"),
+     contactForm = document.querySelector(".contact-form"),
+     getMode = localStorage.getItem("arsentech-theme"),
+     modeToggler = document.querySelector("#icon"),
+     navLinks = document.querySelectorAll("#navLinks a"),
+     frmSearch = document.querySelector(".search-container"),
+     cancelSearchBtn = document.querySelector("#cancel-search");
+let chosenImg,chosenSize,isSizeActive = false;
 if(getMode && getMode === "dark") {document.body.classList.add("dark");modeToggler.querySelector("img").src = "files/icons/dark.svg";}
 lazyCss("https://fonts.googleapis.com/css2?family=Sora:wght@300;400;500;700&display=swap");
 lazyCss("css/dark-mode.css");lazyJS("js/firebase.js");displayCards(downloads,downloadsContainer);
-window.addEventListener("scroll", ()=>window.scrollY>20?navbar.classList.add( "sticky" ):navbar.classList.remove( "sticky" ));
-toggler.addEventListener("click",toggleActive);modeToggler.addEventListener("click", toggleMode);
+window.addEventListener("scroll", ()=>window.scrollY>20?navbar.classList.add("sticky"):navbar.classList.remove("sticky"));
+toggler.addEventListener("click",()=> toggleActive(toggler,navMenu));modeToggler.addEventListener("click", ()=>toggleMode(modeToggler));
+navLinks.forEach(link=>link.addEventListener("click", ()=>closeMenu(toggler,navMenu)));
 document.getElementById("yearCount").innerHTML=new Date().getFullYear();
-function toggleActive(){toggler.classList.toggle("active"); navMenu.classList.toggle("active");}
-function toggleMode(){
-     document.body.classList.toggle("dark");
-     if(!document.body.classList.contains("dark")){modeToggler.querySelector("img").src = "files/icons/light.svg";localStorage.setItem("arsentech-theme", "light");} 
-     else {modeToggler.querySelector("img").src = "files/icons/dark.svg";localStorage.setItem("arsentech-theme", "dark");}
-}
-function closeMenu(){toggler.classList.remove("active"); navMenu.classList.remove("active");}
 const downloadBtns=document.querySelectorAll(".card-btn"),cards = document.querySelectorAll(".grid-item"),list = document.querySelectorAll(".list");
 cards.forEach(card=>{const files = card.dataset.filePath,i = card.dataset.index;getFileInfo(files,i);})
 list.forEach(el=>el.addEventListener("click", ()=>{
-     for(let i=0; i<list.length; i++) list[i].classList.remove("active");
-     el.classList.add("active");const dataFilter = el.dataset.filter;
+     for(let i=0; i<list.length; i++) list[i].classList.remove("active");el.classList.add("active");const dataFilter = el.dataset.filter;
      cards.forEach(card => {card.classList.remove("active");card.classList.add("hide");if(card.dataset.item==dataFilter||dataFilter=="all"){card.classList.remove("hide");card.classList.add("active");}});
 }));
 downloadBtns.forEach(el => {const filePath = el.dataset.filePath;const image = el.querySelector("img");el.addEventListener("click", ()=>downloadFile(filePath, image))});
-tabHeaderBtns.forEach((tab, index)=>tab.addEventListener("click", ()=>{
-     tabs.forEach(content => content.classList.remove("active"));tabHeaderBtns.forEach(tab => tab.classList.remove("active"));tabs[index].classList.add("active");tabHeaderBtns[index].classList.add("active");
-}));
-downloadOptions.forEach((el, index) => {
-     const ids = ["download-option1", "download-option2", "download-option3", "download-option4", "download-option5"];const opt = document.querySelectorAll(ids.map(id => `#${id}`).join(", "));
-     opt[index].addEventListener("change", (e) => handleChange(e, index));
-     el.addEventListener("submit", (e)=> handleSubmit(e, index));
-});
-allStar.forEach((elem, idx)=>elem.addEventListener("click", ()=> {
-     let click = 0;ratingValue.value = idx+1;
+wallpaperOptions.addEventListener("change",(e)=>{chosenImg = changeWallpaper(e.target.value);if(e.target.value) {isSizeActive = true;sizeOptions.removeAttribute("disabled")}if(e.target.value && chosenSize)changeSizeBasedOn(chosenSize,chosenImg-1)})
+sizeOptions.addEventListener("change", (e)=>{if(isSizeActive)changeSize(e,chosenImg-1);if(isSizeActive && e.target.value !== "") chosenSize = e.target.value;})
+frmWallpaper.addEventListener("submit",(e)=>{e.preventDefault();const filePath = `wallpapers/coding${chosenImg-1}/${frmWallpaper.optSize.value}.png`;downloadWallpaper(filePath);})
+allStar.forEach((elem, id)=>elem.addEventListener("click", ()=> {
+     let click = 0;ratingValue.value = id+1;
      allStar.forEach(el => {el.querySelector("img").src="files/icons/star.svg";el.classList.remove("active");});
-     for (let i = 0; i < allStar.length; i++) {if(i <= idx){allStar[i].querySelector("img").src="files/icons/star-fill.svg";allStar[i].classList.add("active");} else {allStar[i].style.setProperty("--i", click);click++;}}
+     for (let i = 0; i < allStar.length; i++) {if(i <= id){allStar[i].querySelector("img").src="files/icons/star-fill.svg";allStar[i].classList.add("active");} else {allStar[i].style.setProperty("--i", click);click++;}}
 }));
 contactForm.addEventListener("submit", (e)=> {
      e.preventDefault();let isValid = false;
      if(ratingValue.value == 0){ratingError.innerHTML = "Required";isValid = false;}else{ratingError.innerHTML = "";isValid = true;}
      if(isValid){contactForm.action = "https://formspree.io/f/mvodlpyz";contactForm.submit();e.target.reset()}
 });
-navLinks.forEach(link=>link.addEventListener("click", closeMenu));
 frmSearch.addEventListener("submit", (e)=>{
      e.preventDefault();let searchInput = document.querySelector("#search-input").value;
      cards.forEach((el,i)=>{
@@ -63,4 +51,4 @@ frmSearch.addEventListener("submit", (e)=>{
           else {cards[i].classList.remove("active");cards[i].classList.add("hide");}
      })
 })
-cancelSearchBtn.addEventListener("click", ()=>{document.querySelector("#search-input").value = "";cards.forEach((_,i)=>{cards[i].classList.remove("hide");cards[i].classList.add("active")})})
+cancelSearchBtn.addEventListener("click", ()=>{document.querySelector("#search-input").value = "";cards.forEach((_,i)=>{cards[i].classList.remove("hide");cards[i].classList.add("active")})});
